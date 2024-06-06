65fe15f6e2a8dcf80ac102ca540d6d45f9a28b63
Nachi28 <79222622+Nachi28@users.noreply.github.com>
2024-06-07 03:17:38 +0530

    troubleshoot

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,8 +1,10 @@
-// src/socket.js
-
+// client/src/socket.js
 import { io } from 'socket.io-client';
 
-// "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'https://gamevo-server.onrender.com';
+const URL = process.env.NODE_ENV === 'production'
+    ? 'https://gamevo-server.onrender.com'
+    : 'http://localhost:4000';  // or your local server URL during development
 
-export const socket = io(URL);
+export const socket = io(URL, {
+    transports: ['websocket'],
+});
